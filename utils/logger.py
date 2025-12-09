74c3915e24eb3283e1ebc7ca253121839d3844f7
VersatileArashi2005 <yawaimyoaung31@gmail.com>
2025-12-09 09:41:31 +0630

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,4 +1,3 @@
-# utils/logger.py
 import logging
 import os
 from logging.handlers import RotatingFileHandler
@@ -11,8 +10,8 @@ formatter = logging.Formatter(
     datefmt="%Y-%m-%d %H:%M:%S"
 )
 
-def setup_logger(name, log_file, level=logging.INFO):
-    handler = RotatingFileHandler(log_file, maxBytes=5*1024*1024, backupCount=3)
+def setup_logger(name, filename, level=logging.INFO):
+    handler = RotatingFileHandler(filename, maxBytes=5*1024*1024, backupCount=3)
     handler.setFormatter(formatter)
     console = logging.StreamHandler()
     console.setFormatter(formatter)
@@ -22,5 +21,5 @@ def setup_logger(name, log_file, level=logging.INFO):
     logger.addHandler(console)
     return logger
 
-app_logger = setup_logger("app", "logs/app.log", logging.INFO)
-error_logger = setup_logger("error", "logs/errors.log", logging.ERROR)
+app_logger = setup_logger("app", "logs/app.log")
+error_logger = setup_logger("error", "logs/errors.log", level=logging.ERROR)
