f28154d232d5c115963d15000860f00c0718e89c
VersatileArashi2005 <yawaimyoaung31@gmail.com>
2025-12-09 21:00:36 +0630

    Update upload.py

--- handlers/upload.py
+++ handlers/upload.py
@@ -700,7 +700,6 @@ upload_conversation_handler = ConversationHandler(
     conversation_timeout=300,  # 5 minute timeout
     name="upload_conversation",
     persistent=False,
-    per_message=True,
 )
 
 
