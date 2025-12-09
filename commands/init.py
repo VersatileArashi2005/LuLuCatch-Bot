198eb02f6cef275548ac60760a696f4d6dd8c582
VersatileArashi2005 <yawaimyoaung31@gmail.com>
2025-12-09 18:57:18 +0630

    Delete commands directory

--- commands/init.py
+++
@@ -1,6 +0,0 @@
-"""Commands package initialization."""
-from .user_commands import setup_user_commands
-from .admin_commands import setup_admin_commands
-from .upload_commands import setup_upload_commands
-
-__all__ = ['setup_user_commands', 'setup_admin_commands', 'setup_upload_commands']
